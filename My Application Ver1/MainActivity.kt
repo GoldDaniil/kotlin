f498f4e79f3a1873a93f1aa1ec9b9cdaf7c934d9
dgold <golddaniiil13@gmail.com>
2024-05-15 23:11:33 +0300

    Create MainActivity.kt

---
+++ My Application Ver1/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private var isDarkMode = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val mainLayout = findViewById<ConstraintLayout>(R.id.main)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        mainLayout.setOnClickListener {
+            // Переключение цветов при нажатии
+            if (isDarkMode) {
+                mainLayout.setBackgroundColor(Color.WHITE)
+                textView.setTextColor(Color.BLACK)
+            } else {
+                mainLayout.setBackgroundColor(Color.BLUE)
+                textView.setTextColor(Color.WHITE)
+            }
+            isDarkMode = !isDarkMode
+        }
+    }
+}
