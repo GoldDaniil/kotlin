1347780d66a050cfed0c960d678db3b7ae68ee56
dgold <golddaniiil13@gmail.com>
2024-05-19 00:53:24 +0300

    Create SearchResultActivity.kt

---
+++ My Application Ver1/SearchResultActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SearchResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_result)
+    }
+}
+
+//import androidx.activity.enableEdgeToEdge
+//class SearchResultActivity : AppCompatActivity() {
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_search_result)
+//    }
+//}
