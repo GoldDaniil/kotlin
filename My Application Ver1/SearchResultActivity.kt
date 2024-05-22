b510509fdf3473a1e9877001a35da667176eef75
dgold <golddaniiil13@gmail.com>
2024-05-22 23:15:23 +0300

    Update SearchResultActivity.kt

--- My Application Ver1/SearchResultActivity.kt
+++ My Application Ver1/SearchResultActivity.kt
@@ -1,20 +1,57 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_result)
+
+        val imageView1 = findViewById<ImageView>(R.id.imageView1)
+        val textView1 = findViewById<TextView>(R.id.textView1)
+
+        val imageView2 = findViewById<ImageView>(R.id.imageView2)
+        val textView2 = findViewById<TextView>(R.id.textView2)
+
+        val imageView3 = findViewById<ImageView>(R.id.imageView3)
+        val textView3 = findViewById<TextView>(R.id.textView3)
+
+        val imageView4 = findViewById<ImageView>(R.id.imageView4)
+        val textView4 = findViewById<TextView>(R.id.textView4)
+
+        val imageView5 = findViewById<ImageView>(R.id.imageView5)
+        val textView5 = findViewById<TextView>(R.id.textView5)
+
+        imageView1.setOnClickListener {
+            toggleTextViewVisibility(textView1)
+        }
+
+        imageView2.setOnClickListener {
+            toggleTextViewVisibility(textView2)
+        }
+
+        imageView3.setOnClickListener {
+            toggleTextViewVisibility(textView3)
+        }
+
+        imageView4.setOnClickListener {
+            toggleTextViewVisibility(textView4)
+        }
+
+        imageView5.setOnClickListener {
+            toggleTextViewVisibility(textView5)
+        }
+    }
+
+    private fun toggleTextViewVisibility(textView: TextView) {
+        if (textView.visibility == View.VISIBLE) {
+            textView.visibility = View.GONE
+        } else {
+            textView.visibility = View.VISIBLE
+        }
     }
 }
-
-//import androidx.activity.enableEdgeToEdge
-//class SearchResultActivity : AppCompatActivity() {
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_search_result)
-//    }
-//}
