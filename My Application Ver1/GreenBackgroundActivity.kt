3b4403d93fed4f5f82fbfe577e66562983534375
dgold <golddaniiil13@gmail.com>
2024-05-23 00:43:16 +0300

    Create GreenBackgroundActivity.kt

---
+++ My Application Ver1/GreenBackgroundActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GreenBackgroundActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.green_background_layout)
+    }
+}
