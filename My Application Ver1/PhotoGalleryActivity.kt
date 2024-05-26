36f08674a7cd101d591ad5a783dbbe9c86c56dc2
dgold <golddaniiil13@gmail.com>
2024-05-26 23:16:49 +0300

    Create PhotoGalleryActivity.kt

---
+++ My Application Ver1/PhotoGalleryActivity.kt
@@ -0,0 +1,50 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+
+class PhotoGalleryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_gallery)
+
+        val viewPager = findViewById<ViewPager>(R.id.viewPager)
+        val adapter = PhotoPagerAdapter(this)
+        viewPager.adapter = adapter
+    }
+
+    private inner class PhotoPagerAdapter(private val context: Context) : PagerAdapter() {
+        private val images = arrayOf(R.drawable.photo1, R.drawable.photo2, R.drawable.photo3, R.drawable.photo4,
+            R.drawable.photo5, R.drawable.photo6, R.drawable.photo7, R.drawable.photo8, R.drawable.photo9, R.drawable.photo10,
+            R.drawable.photo11, R.drawable.photo12)
+
+        override fun getCount(): Int {
+            return images.size
+        }
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            val inflater = LayoutInflater.from(context)
+            val view = inflater.inflate(R.layout.item_photo, container, false)
+            val imageView = view.findViewById<ImageView>(R.id.imageView)
+            imageView.setImageResource(images[position])
+            container.addView(view)
+            return view
+        }
+
+        override fun isViewFromObject(view: View, `object`: Any): Boolean {
+            return view === `object`
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            container.removeView(`object` as View)
+        }
+    }
+}
