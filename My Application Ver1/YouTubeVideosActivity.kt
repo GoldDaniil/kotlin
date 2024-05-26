a145a8df5771e36f912aaa63d022501c1f604861
dgold <golddaniiil13@gmail.com>
2024-05-26 20:18:35 +0300

    Create YouTubeVideosActivity.kt

---
+++ My Application Ver1/YouTubeVideosActivity.kt
@@ -0,0 +1,49 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+class YouTubeVideosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube_videos)
+
+        val countrySpinner = findViewById<Spinner>(R.id.countrySpinner)
+        val watchButton = findViewById<Button>(R.id.watchButton)
+
+        val countries = arrayOf("Бахрейн", "Австралия", "Италия")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, countries)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        countrySpinner.adapter = adapter
+
+        countrySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedCountry = parent.getItemAtPosition(position).toString()
+                val videoId = when (selectedCountry) {
+                    "Бахрейн" -> R.raw.video_grand_prix_bahrain
+                    "Австралия" -> R.raw.video_grand_prix_australia
+                    "Италия" -> R.raw.video_grand_prix_italy
+                    else -> R.raw.gradvideopast
+
+                }
+                watchButton.setOnClickListener {
+                    val intent = Intent(this@YouTubeVideosActivity, FullScreenVideoActivity::class.java)
+                    intent.putExtra("videoId", videoId)
+                    startActivity(intent)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // обработка - если ничего не выбрано
+            }
+        }
+    }
+}
