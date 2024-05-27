e60fdd24ab6a872d192a43507330f1ef897d3775
dgold <golddaniiil13@gmail.com>
2024-05-27 18:16:07 +0300

    Update PhotoGalleryActivity.kt

--- My Application Ver1/PhotoGalleryActivity.kt
+++ My Application Ver1/PhotoGalleryActivity.kt
@@ -24,7 +24,7 @@ class PhotoGalleryActivity : AppCompatActivity() {
     private inner class PhotoPagerAdapter(private val context: Context) : PagerAdapter() {
         private val images = arrayOf(R.drawable.photo1, R.drawable.photo2, R.drawable.photo3, R.drawable.photo4,
             R.drawable.photo5, R.drawable.photo6, R.drawable.photo7, R.drawable.photo8, R.drawable.photo9, R.drawable.photo10,
-            R.drawable.photo11, R.drawable.photo12)
+            R.drawable.photo11, R.drawable.photo12, R.drawable.photo13, R.drawable.photo14, R.drawable.photo15,  R.drawable.photo16,  R.drawable.photo17)
 
         override fun getCount(): Int {
             return images.size
