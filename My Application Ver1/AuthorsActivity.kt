204ee21e43949a52a4e0042f3db8cfcd85266194
dgold <golddaniiil13@gmail.com>
2024-06-02 23:24:39 +0300

    Update AuthorsActivity.kt

--- My Application Ver1/AuthorsActivity.kt
+++ My Application Ver1/AuthorsActivity.kt
@@ -1,7 +1,10 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,8 +14,26 @@ class AuthorsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_authors)
 
         val authorsTextView = findViewById<TextView>(R.id.authorsTextView)
+        val imageContainer = findViewById<View>(R.id.imageContainer)
 
-        val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
-        authorsTextView.startAnimation(animation)
+        val textAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        authorsTextView.startAnimation(textAnimation)
+
+        val containerAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in_delayed)
+        containerAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {}
+            override fun onAnimationEnd(animation: Animation?) {
+                findViewById<ImageView>(R.id.imageView1).apply {
+                    visibility = View.VISIBLE
+                    startAnimation(AnimationUtils.loadAnimation(this@AuthorsActivity, R.anim.fade_in))
+                }
+                findViewById<ImageView>(R.id.imageView2).apply {
+                    visibility = View.VISIBLE
+                    startAnimation(AnimationUtils.loadAnimation(this@AuthorsActivity, R.anim.fade_in))
+                }
+            }
+        })
+        imageContainer.startAnimation(containerAnimation)
     }
 }
