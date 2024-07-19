c60ce3616ead093048ac5e44220d4d6528a1f3eb
dgold <golddaniiil13@gmail.com>
2024-07-19 12:00:05 +0300

    Update ViewPager2.kt

--- My Application Ver1/ViewPager2.kt
+++ My Application Ver1/ViewPager2.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 
 class ViewPager2 : RecyclerView.Adapter<ViewPager2.ViewHolder>() {
@@ -19,7 +21,20 @@ class ViewPager2 : RecyclerView.Adapter<ViewPager2.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // настройка представлений внутри блока
+        // Обработка первой страницы
+        if (position == 0) {
+            val context = holder.itemView.context
+            val buttonQuiz = holder.itemView.findViewById<Button>(R.id.button_quiz)
+            val buttonQuizPhoto = holder.itemView.findViewById<Button>(R.id.button_quiz_photo)
+
+            buttonQuiz.setOnClickListener {
+                context.startActivity(Intent(context, QuizActivity::class.java))
+            }
+
+            buttonQuizPhoto.setOnClickListener {
+                context.startActivity(Intent(context, QuizGameActivity::class.java))
+            }
+        }
     }
 
     override fun getItemCount(): Int = layouts.size
