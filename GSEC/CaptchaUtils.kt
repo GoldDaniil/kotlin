c338a5b6c52b90139c731e8340fc348988f62ecd
dgold <golddaniiil13@gmail.com>
2024-10-08 13:08:27 +0300

    Update CaptchaUtils.kt

--- GSEC/CaptchaUtils.kt
+++ GSEC/CaptchaUtils.kt
@@ -2,7 +2,6 @@ package com.example.goldsecure.utils
 
 object CaptchaUtils {
     fun verifyCaptcha(captcha: String): Boolean {
-        // Implement your captcha validation logic here
         return captcha == "expected_captcha_value"
     }
 }
